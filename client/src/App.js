691ec764a509d8ae84fa36198841a988a5927c75
EricChukoltyansky <kazohelmet@gmail.com>
2022-02-17 11:08:48 +0200

    give basic styling with styled-components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import Landing from "./pages/Landing";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: red;
+  color: white;
+  font-size: 1rem;
+`;
 
 function App() {
   return (
     <>
+      <Button>Click me</Button>
       <h2>Welcome to jobify</h2>
       <Landing />
     </>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,35 @@
 import React from "react";
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
 
 const Landing = () => {
-  return <h1>Landing page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby williamsburg affogato narwhal cold-pressed, forage drinking
+            vinegar vape XOXO slow-carb echo park tacos. Cornhole portland
+            vaporware yr cred knausgaard before they sold out VHS next level
+            small batch. Small batch waistcoat meh cardigan, freegan drinking
+            vinegar celiac YOLO normcore sustainable venmo etsy adaptogen.
+            Brunch chambray messenger bag, kickstarter man braid poke
+            flexitarian pug kinfolk coloring book venmo. Messenger bag lomo
+            yuccie iPhone actually. Vice twee kogi cardigan, brunch whatever
+            coloring book keytar banh mi meditation bitters cray.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
