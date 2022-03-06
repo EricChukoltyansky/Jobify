d33b6b2d0764f98855a81fd0cd454af14dad7359
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-06 12:23:42 +0200

    create custom error handler

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,28 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
 const register = async (req, res, next) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.create({ name, email, password });
   res.status(StatusCodes.OK).json({ user });
 };
 const login = async (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,7 +4,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong, try again later",
+    msg:err.message || "Something went wrong, try again later",
   };
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
