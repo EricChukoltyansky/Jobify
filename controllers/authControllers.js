9bc091cacadb0cd4714ee065de07f1766bcb6652
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-06 12:46:51 +0200

    refactor error handling

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,25 +1,18 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
-
-class CustomAPIError extends Error {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
-
-class BadRequestError extends CustomAPIError {
-  constructor(message) {
-    super(message);
-    this.statusCode = StatusCodes.BAD_REQUEST;
-  }
-}
+import { BadRequestError, NotFoundError } from "../errors/index.js";
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     throw new BadRequestError("Please provide all values");
+  }
+
+  const userAlreadyExists = await User.findOne({ email: email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already exists");
   }
 
   const user = await User.create({ name, email, password });
