1ce3a14f7d58a9a47a440b8e4c034d9d64695287
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-10 18:50:31 +0200

    connect server to front end

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function Dashboard(props) {
+function Dashboard() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/api/v1/users");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <div>Dashboard</div>;
 }
 
