5cc356669119c843ae2a9671b175fed5e782cc5f
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-19 20:40:38 +0200

    small side bar function

--- client/src/components/SmallSideBar.js
+++ client/src/components/SmallSideBar.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { useAppContext } from "../context/appContext";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
 
 export default function SmallSideBar() {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>SmallSideBar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links"></div>
+        </div>
+      </div>
     </Wrapper>
   );
 }
