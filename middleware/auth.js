8ae5c4a1b7ceeeeafa459aae451246ab91c34d90
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-21 18:10:39 +0200

    authenticate user

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,19 @@
+import { UnauthenticatedError } from "../errors/index.js";
+
 const authenticateUser = async (req, res, next) => {
-  console.log("auth middleware");
-  next();
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
+  
+  const token = authHeader.split(" ")[1];
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    throw new UnauthenticatedError("Authentication Invalid");
+  }
 };
 
 export default authenticateUser;
