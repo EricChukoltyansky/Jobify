9d833a64549de488b609bbb4875f6d9eb52f5043
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-22 18:22:01 +0200

    create update user function

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -56,7 +56,34 @@ const login = async (req, res) => {
   }
 };
 const updateUser = async (req, res) => {
-  res.send("updateUser");
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+
+  const token = user.createJWT();
+
+  if (!user) {
+    throw new NotFoundError("User not found");
+  }
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
+    location: user.location,
+  });
 };
 
 export { register, login, updateUser };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,21 @@
-import { UnauthenticatedError } from "../errors/index.js";
+import { UnAuthenticatedError } from "../errors/index.js";
+import jwt from "jsonwebtoken";
 
 const authenticateUser = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    throw new UnauthenticatedError("Authentication Invalid");
+  console.log(authHeader);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnAuthenticatedError("Authentication Invalid");
   }
-  
+
   const token = authHeader.split(" ")[1];
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decodedToken;
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // req.user = payload;
+    req.user = { userId: payload.userId };
     next();
   } catch (err) {
-    throw new UnauthenticatedError("Authentication Invalid");
+    throw new UnAuthenticatedError("Authentication Invalid");
   }
 };
 
