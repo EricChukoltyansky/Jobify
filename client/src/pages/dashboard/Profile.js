dd767016b7904584e78659ce83c675d021d69cc2
EricChukoltyansky <kazohelmet@gmail.com>
2022-03-25 13:12:58 +0300

    set values for form in profile page

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -11,20 +11,59 @@ function Profile() {
   const [email, setEmail] = useState(user?.email);
   const [password, setPassword] = useState(user?.password);
   const [location, setLocation] = useState(user?.location);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!email || !password || !name || !location) {
+      displayAlert();
+      return;
+    }
+    updateUser({
+      name,
+      email,
+      password,
+      location,
+    });
+  };
   return (
     <Wrapper>
-      <div className="form-container">
-        <div className="form-header">
-          <h1>Profile</h1>
-        </div>
-        <div className="form-body">
+      <form action="" className="form" onSubmit={handleSubmit}>
+        <h3>Profile</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
           <FormRow
             label="Name"
+            type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-      </Wrapper>
-      );
+          <FormRow
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <FormRow
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FormRow
+            label="Location"
+            type="text"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={isLoading}
+          />
+        </div>
+      </form>
+    </Wrapper>
+  );
 }
 
 export default Profile;
